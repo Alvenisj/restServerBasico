8cc1e78606103c413e17d909fbbab6af39763fb4
ALVENIS BECERRA <58892711+Alvenisj@users.noreply.github.com>
2024-06-10 17:04:05 -0500

    Update server.js

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ import isRouter from "../routes/user-routes.js";
 export class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT || 3000;
     this.usuarioPath = "/api/usuario";
 
     // Middlewares
